3d8ddb3726f1679993bae39937703b9fe39bfae2
Dyumes <76482455+Dyumes@users.noreply.github.com>
2025-03-24 12:41:42 +0100

    Vigenere and shift modifications

--- encryption.py
+++ encryption.py
@@ -4,40 +4,57 @@ def encrypt_shift(msg, key):
         new_msg += chr(ord(c)+int(key))
     return "".join(new_msg)
 
-def encrypt_vigenere(msg, key):
-    encrypted_text = []
+def generate_key_vig(msg: str, key: str) -> str:
+    key = (key * (len(msg) // len(key) + 1))[:len(msg)]
+    return key
+
+def encrypt_vigenere(msg: str, key: str) -> str:
+    encrypted_text = bytearray()
+    key = generate_key_vig(msg, key)
+    
     for i in range(len(msg)):
-        char = msg[i]
-        if char.isupper():
-            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
-        elif char.islower():
-            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))
-        else:
-            encrypted_char = char
-        encrypted_text.append(encrypted_char)
-    return "".join(encrypted_text)
+        encrypted_byte = (ord(msg[i]) + ord(key[i])) % 256  
+        encrypted_text.append(encrypted_byte)
+    
+    return encrypted_text.decode("latin1")  
+
 
 def decrypt_vigenere(msg, key):
     decrypted_text = []
+    key = generate_key_vig(msg, key)
     for i in range(len(msg)):
         char = msg[i]
         if char.isupper():
-            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
+            decrypted_char = chr((ord(char) - ord(key[i])) % 2**32)
         elif char.islower():
-            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
+            decrypted_char = chr((ord(char) - ord(key[i])) % 2**32)
         else:
-            decrypted_char = char
+            decrypted_char = chr((ord(char) - ord(key[i])) % 2**32)
         decrypted_text.append(decrypted_char)
     return "".join(decrypted_text)
 
-# text_to_encrypt = "abcdefghijklmnopqrstuvwxyz 0123456789 ¨è$àé-.,<"
 
-# key = "TEST"
+#######################
+#TEST
+#######################
 
-# encrypted_text = encrypt_vigenere(text_to_encrypt, key)
+"""""""""""
+text_to_encrypt = "abcdefghijklmnopqrstuvwxyzéà$è¨,.-<12345678890+*ç%&/()=?"
 
-# print(f"Encrypted text: {encrypted_text}")
+key = "cywjAM3I1"
 
-# decrypted_text = decrypt_vigenere(encrypted_text, key)
+encrypted_text = encrypt_vigenere(text_to_encrypt, key)
 
-# print(f"Decrypted text : {decrypted_text}")
+print(f"Encrypted text: {encrypted_text}")
+
+decrypted_text = decrypt_vigenere(encrypted_text, key)
+
+print(f"Decrypted text : {decrypted_text}")
+
+msg = "abcdefghijklmnopqrstuvwxyz1234567890+*ç%&/()=ü!ä£"
+key = 12
+
+encoded = encrypt_shift(msg, 12)
+
+print(encoded)
+"""
